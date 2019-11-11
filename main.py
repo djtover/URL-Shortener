1fda110fdbb6eff76441398656f4dd3069776dde
David <djtover94@gmail.com>
2019-11-11 16:17:26 +0200

    working on sqlite

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
+
+
+
+
+
 
 app = Flask(__name__)
-
+# host = 'http://localhost:'
 @app.route('/')
 def home():
     return render_template('home.html')
