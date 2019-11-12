2d4339ed9736f3d7a7b9171d32787b10b2de66cf
David <djtover94@gmail.com>
2019-11-12 17:57:04 +0200

    Added method to add to db

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import sqlite3
-from sqlite3 import Error
+
+from sqlite3 import OperationalError
+import string
+try:
+    from urllib.parse import urlparse
+    str_encode = str.encode
+except Error:
+    str_encode = str
 
 
-
+host = 'http://localhost:5000/'
 
 # This a method to check if the table has been created
 # if it hasn't then it will create the table URLS
@@ -14,20 +21,38 @@ def table_check():
         URL TEXT NOT NULL
         );
         """
-    with sqlite3.connect('url.db') as conn:
+    with sqlite3.connect('urls.db') as conn:
         cursor = conn.cursor()
         try:
             cursor.execute(create_table)
-        except Error:
+        except OperationalError:
             pass
+
+
+
+
 
 
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        original = str_encode(request.form.get('url'))
+        if urlparse(original).scheme == '':
+            url = 'http://' + original
+        else:
+            url = original
+        with sqlite3.connect('urls.db') as conn:
+            cursor = conn.cursor()
+            res = cursor.execute(
+                'INSERT INTO URLS (URL) VALUES (?)',
+                [original]
+            )
+            index = res.lastrowid
+        return render_template('home.html', short_url=host + index)
     return render_template('home.html')
 
 if __name__ == '__main__':
